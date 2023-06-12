915f0696cca749beaecb8a4d2bd44c3041a01c1d
d.durando <dante.durando@gmail.com>
2023-06-12 12:02:34 -0300

    Rama1

---
+++ Ejemplo1.js
@@ -0,0 +1,6 @@
+console.log("hola git")
+// comandos mas usados en Git 
+// git add . para agregar todos los archivos a git 
+// git commit - masaPizza("este es un comentario") 
+// git push para cargar los archivos al repositorio
+// git pull para traer los archivos del repositorio
